67e315e9b76ac34758214cd5050cae93f3b02a5d
Akos Kitta <kittaakos@typefox.io>
2018-08-02 16:43:13 +0200

    Released a new version.
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 
 rm -rf ./lsp4j/ \
 && git clone --single-branch -b GH-197 https://github.com/kittaakos/lsp4j.git \
-&& ./lsp4j/gradlew -p lsp4j build generateP2Build --refresh-dependencies \
+&& ./lsp4j/gradlew -p lsp4j build install generateP2Build --refresh-dependencies \
 && mvn -f ./lsp4j/releng/pom.xml clean verify \
 && rm -rf ./p2/ \
 && unzip -o ./lsp4j/releng/p2/target/lsp4j.p2-repository-0.5.0-SNAPSHOT.zip -d ./p2/
